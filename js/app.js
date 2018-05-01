c0e3cc6603344622420c91e60e14bdfd83d29598
Diane Pelletrat de Borde <dianepelletratdeborde@dibenn.com>
2018-05-01 16:10:42 +0200

    modified the cardIsClicked function : removed the duplicates with the compiled function, and adedd 'this' as the elment to push in the ClickedCardArray when a card is clicked.

--- js/app.js
+++ js/app.js
@@ -58,24 +58,22 @@ function restartGame () {
 let clickedCardArray=[];//creates an empty array; to store the clicked cards. I use let instead of const, otherwise I will not be able to empty it by resetting its value. 
 let matchedCardsArray=[];// /creates an empty array; to store the matched cards.
 let arrayLength=clickedCardArray.length
-let firstClickedcard=clickedCardArray[0].innerHTML
-let secondClickedcard=clickedCardArray[1].innerHTML
+
 
 
 // function that shows the cards and their symbols 
  function cardIsClicked (){
-   cardArray.forEach(function(card){ // I found this method much easier than the classical one 
-   card.addEventListener('click',function(){
        this.classList.toggle('open'); // changes the background color of the card from black to blue 
        this.classList.toggle('show'); // make the symbols show
-       clickedCardArray.push(card); // stores the value of the clicked cards in an array
-   });
-});
-}
+       clickedCardArray.push(this); // stores the value of the clicked cards in an array
+};
+
 
 
 // function that compares cards values 
 function manageClickedCards (){
+	let firstClickedcard=clickedCardArray[0].innerHTML
+	let secondClickedcard=clickedCardArray[1].innerHTML
 if (arrayLength === 2 && firstClickedcard!=secondClickedcard) {
   clickedCardArray[0].classList.toggle('open');
   clickedCardArray[0].classList.toggle('show');
@@ -96,10 +94,9 @@ else if (arrayLength === 2 && firstClickedcard===secondClickedcard){
 
 
 
-
 // function that is compiling the other functions, and store them to be invoked when a card is clicked
 cardArray.forEach(function(card) {
-	card.addEventListener('click',cardIsClicked);
+  card.addEventListener('click',cardIsClicked);
   	card.addEventListener('click',manageClickedCards);
    });
 
@@ -108,11 +105,6 @@ cardArray.forEach(function(card) {
 
 
 
-// 6. Flip the card 
-//Toggle class
-
-// 7. Check those variables for match class
-//if class===class  
 
 
 /*
