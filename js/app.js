744ff88418ccc1222de34ec2c8ed38f0b5f84cd7
Diane Pelletrat de Borde <dianepelletratdeborde@dibenn.com>
2018-05-02 21:27:32 +0200

    Created the modal (pop-up). But it still has to show only if the matchedCardArray length is 16.

--- js/app.js
+++ js/app.js
@@ -125,73 +125,13 @@ cardArray.forEach(function(card) {
   	card.addEventListener('click',manageClickedCards);
    });
 
-
-
-
-
-
-
-
-
-/*
-*
-Hourray ! After hours of trying, I eventually made it ! 
-Now, when I click on the card, the class 'open' is added, the symbol shows, and the card is added to a new array. 
-Also, I'll have to store this in a function, to be called right after the restart game function.  
-*
-*/
-// I tried to wrap it up in a function (line 121)
-/*cardArray.forEach(function(card){ // I found this method much easier than the classical one 
- card.addEventListener('click',function(){
-      this.classList.toggle('open'); // changes the background color of the card from black to blue 
-      this.classList.toggle('show'); // make the symbols show
-      clickedCardArray.push(card); // stores the value of the clicked cards in an array. I have to find a way to limit the maxiumum number of holded cards to 2. 
-   });
-});
-
-//
-
-let arrayLength=clickedCardArray.length
-let firstClickedcard=clickedCardArray[0].innerHTML
-// 3. Store the first element clicked in a variable and start the counter with this first click, a recursive function (one calling itself) will do this with setTimeout.
-let secondClickedcard=clickedCardArray[1].innerHTML
-// 5.  Second click and store this element as well in another variable.
-// I tried to wrap this up in a function (line 103)
-if (arrayLength === 2 && firstClickedcard!=secondClickedcard) {
-  clickedCardArray[0].classList.toggle('open');
-  clickedCardArray[0].classList.toggle('show');
-  clickedCardArray[1].classList.toggle('open');
-  clickedCardArray[1].classList.toggle('show');
-  clickedCardArray = [];// remove all cards from the array, seems to work better than splice method. 
-}
-else if (arrayLength === 2 && firstClickedcard===secondClickedcard){
-  clickedCardArray[0].classList.toggle('match');
-  clickedCardArray[1].classList.toggle('match');
-  // I should push those cards in a new array 'Matchedcards';
-  clickedCardArray.forEach(function(card){
-  matchedCardsArray.push(card); 
-  clickedCardArray = [];// remove all cards from the array, seems to work better than splice method. 
+// function that is making the pop-up show at the end of the game 
+if (matchedCardsArray.length===16){
+  modal.style.display = "block"; // source = https://www.w3schools.com/howto/howto_css_modals.asp
 }
 
-*/
-
-
- 
-//  13. For the reset button, a button in html with an event listener that calls a function to start the game.
-//const endOfGame =document.querySelector('#endOfGame');
-//endOfGame.addEventListener('click',function(){
-  //  console.log("a button was clicked"); // function to be changed of course. This is just the starting point. 
-//});
-
-
-// The popup can be section in html with display hidden that changes to visible once the matches are 8.
-
-
- /* - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// function that is closing the pop up
+var span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
+span.onclick = function() { // When the user clicks on <span> (x), close the modal
+    modal.style.display = "none";
+}
